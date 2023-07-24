262a77b7618cebf6dd246de63d04eb93f5918230
Lachlan Young <129141566+loc-codes@users.noreply.github.com>
2023-07-25 09:58:42 +1000

    Added CS61A week1

---
+++ CS61A/week1/disc01_Q6.py
@@ -0,0 +1,34 @@
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> result is None  # No return value
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    i = 1
+    while i <= n:
+        if i % 15 == 0:
+            print("fizzbuzz")
+        elif i % 5 == 0:
+            print("buzz")
+        elif i % 3 == 0:
+            print("fizz")
+        else:
+            print(i)
+        i += 1
