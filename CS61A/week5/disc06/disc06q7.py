d3d504d421a54fe77d9d81d6e68704ca787a44f6
Lachlan Young <129141566+loc-codes@users.noreply.github.com>
2023-08-02 08:52:50 +1000

    Completed disc06

---
+++ CS61A/week5/disc06/disc06q7.py
@@ -0,0 +1,67 @@
+class Pet:
+
+    def __init__(self, name, owner):
+        self.is_alive = True    # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+class Cat(Pet):
+    def __init__(self, name, owner, lives=9):
+        super().__init__(name,owner)
+        self.lives = lives
+
+    def talk(self):
+        """Print out a cat's greeting.
+
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        print(f'{self.name} says meow!')
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero,
+        is_alive becomes False. If this is called after lives has
+        reached zero, print 'This cat has no more lives to lose.'
+        """
+        self.lives -= 1
+        if self.lives == 0:
+            self.is_alive = False
+
+    def revive(self):
+        """Revives a cat from the dead. The cat should now have 
+        9 lives and is_alive should be true. Can only be called
+        on a cat that is dead. If the cat isn't dead, print 
+        'This cat still has lives to lose.'
+        """
+        if not self.is_alive:
+            self.lives = 9
+        else:
+            print('This cat still has lives to lose.')
+
+class NoisyCat(Cat): # Fill me in!
+    """A Cat that repeats things twice."""
+    def __init__(self, name, owner, lives=9):
+        # Q: Is this method necessary? Why or why not?
+        # A: This method is not necessary, we could just call Cat.__init__(name,owner,lives). 
+        # We can call this explicitly, or just not have an init method in NoisyCat
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner,lives)
+
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        for i in range(2):
+            super().talk()
+
+import doctest
+doctest.testmod()
