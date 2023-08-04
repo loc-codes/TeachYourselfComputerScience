c31fb23b80b2f9b5b9c5a37b0ca600b5cff617db
Lachlan Young <129141566+loc-codes@users.noreply.github.com>
2023-08-04 17:12:39 +1000

    Added disc07 - Mutable Tree Qs

---
+++ CS61A/week5/disc07/disc07q2.py
@@ -0,0 +1,22 @@
+from Tree import Tree
+import doctest
+
+def cumulative_mul(t):
+    """Mutates t so that each node's label becomes the product of all labels in
+    the corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_mul(t)
+    >>> t
+    Tree(105, [Tree(15, [Tree(5)]), Tree(7)])
+    >>> otherTree = Tree(2, [Tree(1, [Tree(3), Tree(4), Tree(5)]), Tree(6, [Tree(7)])])
+    >>> cumulative_mul(otherTree)
+    >>> otherTree
+    Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
+    """
+    "*** YOUR CODE HERE ***"
+    for b in t.branches:
+        cumulative_mul(b)
+        t.label *= b.label
+
+doctest.testmod()
