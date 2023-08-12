6c8ce0913e1565410e1f113dec60d887b8d97e5c
Lachlan Young <129141566+loc-codes@users.noreply.github.com>
2023-08-12 14:58:16 +1000

    Completed lab10

--- CS61A/week6/lab10/lab10.scm
+++ CS61A/week6/lab10/lab10.scm
@@ -5,17 +5,34 @@
 (define (my-filter pred s) 
     (cond 
     ((eq? s ()) ())
-    ((pred (car s)) (cons (car s) (filter pred (cdr s))))
-    (else (filter pred (cdr s)))
+    ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+    (else (my-filter pred (cdr s)))
     )
 )
 
 (define (exp b n)
-  (define (helper b n so-far) "YOUR CODE HERE")
+  (define (helper b n so-far)
+    (if (= n 0) 
+    so-far
+    (helper b (- n 1) (* so-far b))
+    )
+  
+  )
   (helper b n 1))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (cond ((and (eq? lst1 nil) (eq? lst2 nil)) nil)
+        ((eq? lst2 nil) lst1)
+        ((eq? lst1 nil) lst2)
+        (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))
+  )
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (cond ((= exp 1) base)
+        ((even? exp) (square (pow base (/ exp 2))))
+        ((odd? exp) (* base (pow base (- exp 1))))
+  )
+)
